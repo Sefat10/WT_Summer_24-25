f7b5477d550bc533741129625edae3e27243d124
Sefat <abdullah.Sefat10@gmail.com>
2025-07-27 13:21:00 +0600

    first commit

---
+++ mid/Lab 01/form.php
@@ -0,0 +1,88 @@
+<! Doctype html>
+<html>
+
+<title>This is Registration form</title>
+<center style="color:blue;">
+   <h1>Aiub</h1></p>
+  <h2>Registration Form</h2></p>
+</center>
+<body>
+<b><h3 align="left">Complete the Registration</h3></b>
+<table>
+<tr> 
+    <td> 
+        Full Name:</td>
+        <td>
+<input type="text"></td>
+</td>
+</tr>
+<tr> 
+    <td> 
+        Email:</td>
+        <td>
+<input type="text"></td>
+</td>
+</tr>
+<tr> 
+    <td> 
+        Password:</td>
+        <td>
+<input type="text"></td>
+</td>
+</tr>
+</table>
+<table>
+<tr>
+    <td>
+        Gender:
+    </td>
+    <td><input type="radio"name="Language">Male</td>
+    <td><input type="radio"name="Language">Other</td>
+</tr>
+<tr>
+    <td>Languages Known:</td>
+    <td><input type="checkbox">English</td>
+    <td><input type="checkbox">Bangla</td>
+    <td><input type="checkbox">Hindi</td>
+  
+
+</tr>
+
+<tr>
+<td>Country:</td>
+<td><select><option value="">--Select Option 
+<option value="">Bangladesh
+<option value="">Japan
+<option value="">USA
+
+</select></td>
+
+
+</tr>
+
+<tr>
+<td>Date of Birth:</td>
+<td><input type="date"></td>
+</tr>
+</table>
+<table>
+    <tr>
+<td>Upload Photo:</td>
+<td><input type="file"></td>
+</tr>
+
+<tr>
+<td>Comments:</td>
+<td><textarea></textarea></td>
+</tr>
+
+</table>
+<tr>
+<td><input type="Submit" value="Register"></td>
+</tr>
+
+
+ 
+
+</body>
+</html>
---
+++ mid/Lab Task 2/view/Style.php
@@ -0,0 +1,138 @@
+<! Doctype html>
+<html>
+<style>
+    body{
+        background-color: lightblue;
+    }
+div {
+  background-color: WHITE;
+  width: 150px;
+  height: 50px;
+  border: 1px solid orange;
+  overflow: scroll;
+}
+</style>
+
+<title>This is Registration Bank form</title>
+<center style="color:blue;">
+   <h1>Bank Management System</h1>
+  <h2>Your Trusted Financial Partner</h2>
+</center>
+<body>
+<b><h3 align="left">Customer the Registration</h3></b>
+<table>
+<tr> 
+    <td> 
+        Full Name:</td>
+        <td>
+<input type="text"></td>
+</td>
+</tr>
+<tr>
+<td>Date of Birth:</td>
+<td><input type="date"></td>
+</tr>
+<tr>
+    <td>
+        Gender:
+    </td>
+    <td><input type="radio"name="Language">Male
+    <input type="radio"name="Language">Female
+    <input type="radio"name="Language">Other</td>
+</tr>
+<tr>
+<td>Marital Status:</td>
+<td><select><option value="">--Select Option 
+<option value="">Single
+<option value="">Maried
+</select></td>
+
+
+</tr>
+<tr>
+<td>Account Type:</td>
+<td><select><option value="">--Select Option 
+<option value="">Saving
+<option value="">Depsite
+</select></td>
+
+<tr> 
+    <td> 
+        Initial Deposite Annount:</td>
+        <td>
+<input type="text"></td>
+</td>
+</tr>
+<tr> 
+    <td> 
+        Mobile Number:</td>
+        <td>
+<input type="text"></td>
+</td>
+</tr>
+
+
+</tr>
+<tr> 
+    <td> 
+        Email Address:</td>
+        <td>
+<input type="text"></td>
+</td>
+</tr>
+<tr>
+<td>Address:</td>
+<td><textarea></textarea></td>
+</tr>
+<tr> 
+    <td> 
+        Occupation:</td>
+        <td>
+<input type="text"></td>
+</td>
+</tr>
+
+<tr> 
+    <td> 
+        National ID (NID):</td>
+        <td>
+<input type="text"></td>
+</td>
+</tr>
+
+<tr> 
+    <td> 
+        Set Password:</td>
+        <td>
+<input type="text"></td>
+</td>
+</tr>
+
+</table>
+
+<table>
+    <tr>
+<td>Upload Photo:</td>
+<td><input type="file"></td>
+</tr>
+
+</table>
+<table>
+
+<tr>
+    
+        <td><input type="checkbox" id="terms"></td>
+        <td><level for="terms">I agree to the term and Condition</td>
+   
+
+</tr>
+</table>
+<tr>
+<td><input type="Submit" value="Register"></td>
+<td><input type="Reset" value="Clear"></td>
+</tr>
+
+<div>This is demo text to show overflow works in a small container with a </div>
+</body>
+
+</html>
